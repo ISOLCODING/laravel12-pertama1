0b23d85caccd206e7ebe29fc2786762604c16032
ISOLCODING <rrqhoshi891@gmail.com>
2025-08-30 02:27:18 +0700

    Remove uniqueness constraint from project description field

--- database/migrations/2025_08_28_144734_create_projects_table.php
+++ database/migrations/2025_08_28_144734_create_projects_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->text('description')->unique();
+            $table->text('description');
             $table->timestamp('start_date')->nullable();
             $table->timestamp('end_date')->nullable();
             $table->string('image_path')->nullable();
